dacad4ec722b1e0471553148db908aa6915e181e
Ead <ead.bunnamon@oivan.com>
2022-06-13 00:58:07 +0700

    restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,9 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :name_en, :open_time, :close_time,
-  :day_off_description, :day_off_description_en, :restaurant_type_id,
-  :active
+  :day_off_description, :day_off_description_en, :active,
+  :restaurant_type
+
+  def restaurant_type
+    ::RestaurantTypeSerializer.new(object.restaurant_type).attributes || {}
+  end
 end
