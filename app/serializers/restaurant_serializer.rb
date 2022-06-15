afe9918cb3cbded148dbf6cd8dfadb84280a2b77
Ead <ead.bunnamon@oivan.com>
2022-06-16 00:32:19 +0700

    Update restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,9 +1,14 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :name_en, :open_time, :close_time,
   :day_off_description, :day_off_description_en, :active,
-  :restaurant_type
+  :restaurant_type, :number_of_tables
 
   def restaurant_type
     ::RestaurantTypeSerializer.new(object.restaurant_type).attributes || {}
+  end
+
+  def number_of_tables
+    return 0 if object.tables.blank?
+    object.tables.count
   end
 end
